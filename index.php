da60c4a949bcbd6a411a30e86f059420d3aea37c
julien <julien>
2022-02-16 14:04:09 +0100

    added files

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <?php
+$mysqli = new mysqli("192.168.0.2","root","root", "testapp");
+if ($mysqli->connect_errno) {
+    echo "Échec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+else {
+    echo "Connexion réussie à MySQL :)";
+}
+?>
+</body>
+</html>
